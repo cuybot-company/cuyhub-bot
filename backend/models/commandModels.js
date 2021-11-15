6b9aa0273a4a5833656cdb761379da28e797d58d
rizsyad <rizsyad@gmail.com>
2021-11-15 15:36:20 +0700

    fix typo

--- backend/models/commandModels.js
+++ backend/models/commandModels.js
@@ -3,10 +3,4 @@ const Schema = mongoose.Schema;
 
 const CommandSchema = new Schema({});
 
-export const addCommand = () => {};
-
-export const getCommand = () => {};
-
-export const updateCommand = () => {};
-
-export const deleteCommand = () => {};
+module.exports = mongoose.model("Command", CommandSchema);
--- backend/models/settingModels.js
+++ backend/models/settingModels.js
@@ -1,9 +1,12 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const SettingSchema = new Schema({
-  name: { type: String, required: true },
-  value: { type: String, required: true },
-});
+const SettingSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    value: { type: String, required: true },
+  },
+  { timestamps: true }
+);
 
-module.exports = mongoose.model("Setting", MessageSchema);
+module.exports = mongoose.model("Setting", SettingSchema);
